b9d3aaedcdc7c1a61660c11e8650d70bed1e692b
jaoviitor <joaoviiitorvieira@gmail.com>
2024-05-28 10:01:17 -0300

    att exception endpoints

--- src/main/java/com/project/api/config/SecurityConfig.java
+++ src/main/java/com/project/api/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,7 +41,7 @@ public class SecurityConfig{
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/api/usuario/login").permitAll()
+					.requestMatchers(HttpMethod.POST, "/api/usuario/login", "/api/usuario").permitAll()
 					.anyRequest().authenticated())
 			.sessionManagement(session -> session
 					.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
